b30e4da4901e4a44b8d5c988cee7bb3cef853744
Oleg Mikhalev <D:\ ()\Effecttools\Effecttools>
2016-06-17 23:37:47 +0300

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+//(function() {
+//  'use strict';
+
+//  setTimeout(function() {
+//    document.querySelector('.greating_picture').classList.add('m--show');
+//  }, 1000);
+//})();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyhmdW5jdGlvbigpIHtcclxuLy8gICd1c2Ugc3RyaWN0JztcclxuXHJcbi8vICBzZXRUaW1lb3V0KGZ1bmN0aW9uKCkge1xyXG4vLyAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcuZ3JlYXRpbmdfcGljdHVyZScpLmNsYXNzTGlzdC5hZGQoJ20tLXNob3cnKTtcclxuLy8gIH0sIDEwMDApO1xyXG4vL30pKCk7Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
